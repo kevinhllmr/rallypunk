2537a4531086f34776a39fb5be2a6e8ffebdcdf8
kevinhllmr <kevin.hollmeier@freenet.de>
2024-06-15 21:01:58 +0200

    shader test

--- RepairShop.gd
+++ RepairShop.gd
@@ -4,6 +4,7 @@ var slowing_down = false
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
 	
 func _on_body_entered(body):
 	if body.is_in_group("car"):
