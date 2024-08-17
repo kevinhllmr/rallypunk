cfe77ad9dbc40534723fcc475799c684bfd8ccfd
Nils Matthias <n.matthias@gmx.de>
2024-08-17 12:23:54 +0200

    Levelauswahl + Rankingssystem + Playerstats

---
+++ Scripts/player_stats.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var scrap = 0
+var xp = 0
+var rank = 0
+var totalXP = 0
+var uprank = (pow(2,rank))*500
+
+
+
+func rankupgrade():
+
+	if xp>=uprank:
+		xp = 0
+		rank += 1
+		
+func addXP(amount):
+	xp =+ amount
+	totalXP =+ amount
+	rankupgrade()
