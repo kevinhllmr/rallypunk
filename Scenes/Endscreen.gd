646249078a25d03124b6da4605cbb764f898bd1a
Nils Matthias <n.matthias@gmx.de>
2024-09-01 13:58:39 +0200

    Update Endscreen.gd

--- Scenes/Endscreen.gd
+++ Scenes/Endscreen.gd
@@ -4,7 +4,7 @@ func _ready():
 	PlayerStats.addXP(10000)
 	print(str(PlayerStats.xp))
 	$Background/Sprite2D/rank.text = "Rank: " + str(PlayerStats.rank)
-	$Background/Sprite2D/XP.text = "XP: " + str(PlayerStats.xp)
+	$Background/Sprite2D/XP.text = "XP: " + str(PlayerStats.totalXP)
 	$Background/ProgressBar.max_value = PlayerStats.uprank
 	$Background/ProgressBar.value = PlayerStats.xp
 	$Background/VBoxContainer/collected_Scrap.text = "Scrap: " + str(PlayerStats.collectedScrap)
